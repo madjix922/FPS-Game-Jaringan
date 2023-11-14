845ee89fabac856ee91f4c1800d5a0a30ecfe6aa
madjix922 <m.majid.akbar@gmail.com>
2023-11-14 14:13:50 +0700

    Update 3

--- FPS coba coba/Assets/Script/Movement.cs
+++ FPS coba coba/Assets/Script/Movement.cs
@@ -8,8 +8,20 @@ public class Movement : MonoBehaviour
     public float sprintSpeed = 14f;
     public float maxVelocityChange = 10f;
 
+    [Space]
+    public float airControl = 0.5f;
+
+    [Space]
+    public float jumpHeight = 5f;
+
     private Vector2 input;
     private Rigidbody rb;
+
+    private bool sprinting;
+    private bool jumping;
+
+    private bool grounded = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +33,51 @@ public class Movement : MonoBehaviour
     {
         input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         input.Normalize();
+
+        sprinting = Input.GetButton("Sprint");
+        jumping = Input.GetButton("Jump");
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        grounded = true;
+    }
+
 
     void FixedUpdate()
     {
-        rb.AddForce(CalculateMovement(walkSpeed), ForceMode.VelocityChange);
+        if (grounded)
+        {
+            if (jumping)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);
+            }
+            else if (input.magnitude > 0.5f)
+            {
+                rb.AddForce(CalculateMovement(sprinting ? sprintSpeed : walkSpeed), ForceMode.VelocityChange);
+            }
+            else
+            {
+                var velocity1 = rb.velocity;
+                velocity1 = new Vector3(velocity1.x * 0.2f * Time.fixedDeltaTime, velocity1.y, velocity1.z * 0.2f * Time.fixedDeltaTime);
+                rb.velocity = velocity1;
+            }
+        }
+        else
+        {
+            if (input.magnitude > 0.5f)
+            {
+                rb.AddForce(CalculateMovement(sprinting ? sprintSpeed * airControl : walkSpeed * airControl), ForceMode.VelocityChange);
+            }
+            else
+            {
+                var velocity1 = rb.velocity;
+                velocity1 = new Vector3(velocity1.x * 0.2f * Time.fixedDeltaTime, velocity1.y, velocity1.z * 0.2f * Time.fixedDeltaTime);
+                rb.velocity = velocity1;
+            }
+        }
+
+        grounded = false;
     }
 
     Vector3 CalculateMovement(float _speed)
